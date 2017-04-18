34f69b861ddb0546d095ca091caee9a3e713438b
Ethan Vizitei <ethan.vizitei@gmail.com>
2017-04-17 20:08:28 -0700

    make tanks draw on the screen

--- src/game.rb
+++ src/game.rb
@@ -6,12 +6,14 @@ require_relative './lib/fence'
 class Tanks < Gosu::Window
   GAME_WIDTH = 960
   GAME_HEIGHT = 720
+  TILE_SIZE = 78
 
   def initialize
     super GAME_WIDTH, GAME_HEIGHT
     self.caption = "Udaci-Tanks!"
     @background_image = Gosu::Image.new("assets/background_scaled.jpg", tileable: true)
-    @fence = Fence.new(Gosu::Image.new("assets/wall.png", tileable: true))
+    @fence = Fence.new(Gosu::Image.new("assets/wall.png", tileable: true), TILE_SIZE)
+    @tank = Gosu::Image.new("assets/tank_purple.png")
   end
 
   def update
@@ -20,6 +22,9 @@ class Tanks < Gosu::Window
   def draw
     @background_image.draw(0,0,0)
     @fence.draw
+    @tank.draw(160, 200, 2, 1.5, 1.5)
+    #@tank.draw(160 + TILE_SIZE, 200, 2, 1.5, 1.5)
+    @tank.draw_rot(160 + TILE_SIZE, 200, 2, 90, 0.5, 0.5, 1.5, 1.5)
   end
 end
 
--- src/lib/fence.rb
+++ src/lib/fence.rb
@@ -1,11 +1,11 @@
-TILE_SIZE = 78
 FENCE_SCALE = 0.8
 OFFSET_X = -13
 OFFSET_Y = 25
 
 class Fence
-  def initialize(image, x_tiles=13, y_tiles=8)
+  def initialize(image, tile_size, x_tiles=13, y_tiles=8)
     @image = image
+    @tile_size = tile_size
     @x_units = x_tiles
     @y_units = y_tiles
   end
@@ -22,33 +22,33 @@ class Fence
     x = OFFSET_X
     @x_units.times do
       draw_tile(x, OFFSET_Y)
-      x += TILE_SIZE
+      x += @tile_size
     end
   end
 
   def draw_left
-    y = OFFSET_Y + TILE_SIZE
+    y = OFFSET_Y + @tile_size
     (@y_units - 1).times do
       draw_tile(OFFSET_X, y)
-      y += TILE_SIZE
+      y += @tile_size
     end
   end
 
   def draw_right
-    x = (OFFSET_X + (TILE_SIZE * (@x_units - 1)))
-    y = OFFSET_Y + TILE_SIZE
+    x = (OFFSET_X + (@tile_size * (@x_units - 1)))
+    y = OFFSET_Y + @tile_size
     (@y_units - 1).times do
       draw_tile(x, y)
-      y += TILE_SIZE
+      y += @tile_size
     end
   end
 
   def draw_bottom
-    x = OFFSET_X + TILE_SIZE
-    y = (OFFSET_Y + (TILE_SIZE * (@y_units - 1)))
+    x = OFFSET_X + @tile_size
+    y = (OFFSET_Y + (@tile_size * (@y_units - 1)))
     (@x_units - 2).times do
       draw_tile(x, y)
-      x += TILE_SIZE
+      x += @tile_size
     end
   end
 
